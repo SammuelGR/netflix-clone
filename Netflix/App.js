c13f0363227ff030150ec3ce13492a4ef043b11c
Sammuel Reis <sammugr@hotmail.com>
2018-12-14 20:02:32 -0200

    fix App.js

--- Netflix/App.js
+++ Netflix/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const App = () => (
+const App = () => (
   <View style={styles.container}>
     <Text>Netflix!</Text>
   </View>
 );
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
