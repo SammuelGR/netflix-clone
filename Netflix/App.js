e5599b287550fea5e2538a2fceaec7220b7e4288
Sammuel Reis <sammugr@hotmail.com>
2018-12-14 12:46:51 -0200

    setup project

---
+++ Netflix/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Netflix!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
